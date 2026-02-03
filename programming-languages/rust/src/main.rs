ab6564ec975c3495ea4eb145fdce48027f4bf115
Matheus de Paula <matheusdev20@gmail.com>
2026-02-03 09:08:56 -0300

    content DDA

--- programming-languages/rust/src/main.rs
+++ programming-languages/rust/src/main.rs
@@ -1,20 +1,39 @@
-
-use std::io;
-
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the Number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your guess");
+    loop {
+        println!("Please input your guess");
+        println!("Type quit anytime to exit the game");
 
-    let mut guess = String::new();     gpa
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read message");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read message");
 
-    println!("You guessed: {guess}");
+        if guess.trim() == "quit" {
+            break;
+        }
 
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal =>  {
+                println!("Yow Win");
+                break;
+            }
+        }
+    }
 }
--- system-design/design-data-intensive-apps.md
+++ system-design/design-data-intensive-apps.md
@@ -1,2 +1,11 @@
 Design Data Intensive Apps
 
+
+Chapter I
+
+Reliability
+  > Fault:  Things that may go wrong with your system, can lead to failures but not necessarily
+  > Failure: Service Stop providing the required service to the user, can be induced by not Handled faults
+
+
+
